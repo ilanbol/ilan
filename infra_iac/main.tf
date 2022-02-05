a51e8787af352b22f7b057fe33f5e318be162b82
ilanbol <ilan_bl@hotmail.com>
2022-02-05 16:31:09 +0200

    jenkins

--- infra_iac/main.tf
+++ infra_iac/main.tf
@@ -9,22 +9,22 @@ data "aws_ssm_parameter" "this" {
 
 
 #Create webserver
-resource "aws_instance" "webserver" {
+resource "aws_instance" "jenkins" {
   ami                         = data.aws_ssm_parameter
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   key_name                    = var.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = var.aws_subnet
   user_data = "${file("config/config.sh")}"
   tags = {
-    Name = "webserver"
+    Name = "jenkins"
   }
 }
 
 
 #Create SG for allowing TCP/80 & TCP/22
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "jenkins-sg" {
   name        = "sg"
   description = "Allow TCP/80 & TCP/22"
   vpc_id      = var.vpc.id
